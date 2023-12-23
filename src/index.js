07965c30719df2f352059fa387fab93faefaa78b
Rafael Zuquim <rzuquim@inoa.com.br>
2023-12-23 09:50:21 -0300

    feat: select chord to add

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 /**
  * Creates a new harmony mate that changes the content of a pre tag with the chords
  * @param {Element} lyricsElement - The <pre> element holding the lyrics
+ * @param {Element} chordsElement - The container element holding the chords
  * @param {Options} options - custom behavior
  * **/
-let new_mate = (lyricsElement, options) => {
+let new_mate = (lyricsElement, chordsElement, options) => {
   if (lyricsElement.tagName !== 'PRE') {
     throw Error('Harmony mate must be bound to a <pre> element.')
   }
 
-  let selectedChord = 'A'
+  let selectedChord = ''
   options ??= defaultOptions
   let lyrics_lines = lyricsElement.textContent.split('\n')
 
+  chordsElement.addEventListener('click', (evt) => {
+    let clickedEl = evt.target
+    if (!clickedEl.classList.contains('chord')) return
+
+    selectedChord = clickedEl.textContent
+    removeClassFromChildren(chordsElement, options.selectedChordClass)
+    clickedEl.classList.add(options.selectedChordClass)
+  })
+
   lyricsElement.addEventListener('click', (evt) => {
+    if (!selectedChord) {
+      if (options.verbose) console.log('no chord selected')
+      return
+    }
+
     // screen position
     let [clickX, clickY] = [evt.clientX, evt.clientY]
 
@@ -36,6 +51,7 @@ let new_mate = (lyricsElement, options) => {
 
     if (options.verbose)
       console.log({
+        selectedChord,
         clickX,
         clickY,
         relativeX,
@@ -102,6 +118,19 @@ let calculateLineWidth = (line, lyricsElement) => {
 }
 
 /**
+ * Recursive function to remove the specified class from the given element and all its descendants
+ * @param {Element} element
+ * @param {String} className
+ **/
+function removeClassFromChildren(element, className) {
+  element.classList.remove(className)
+  var childElements = element.children
+  for (var i = 0; i < childElements.length; i++) {
+    removeClassFromChildren(childElements[i], className)
+  }
+}
+
+/**
  * Checks if line is has lyrics (not chords)
  * @param {String} line
  * @returns {Boolean}
@@ -111,18 +140,25 @@ let isLyricsLine = (line) => /\w{3,}/.test(line) // has any word with 3 or more 
 /**
  * @typedef {Object} Options - options to fine tune the harmony mate behavior.
  * @property {boolean} verbose - should log the memory calculation
+ * @property {String} selectedChordClass - the css class with the style of the selected chord
  */
 let defaultOptions = {
   verbose: false,
+  selectedChordClass: 'is-selected',
 }
 
 /**
  * Creates a new harmony mate that changes the content of the element with the id
- * @param {String} id - the id of the <pre> tag
+ * @param {String} lyricsId - the id of the <pre> tag with the lyrics
+ * @param {String} chordsId - the id of the element holding possible chords
  * @param {Options} options - custom behavior
  * **/
-window.mate_by_id = (id, options) => {
-  let lyricsElements = document.getElementById(id)
+window.mate_by_id = (lyricsId, chordsId, options) => {
+  let lyricsElements = document.getElementById(lyricsId)
+  let chordsElements = document.getElementById(chordsId)
 
-  return new_mate(lyricsElements, options)
+  return new_mate(lyricsElements, chordsElements, {
+    ...defaultOptions,
+    ...options,
+  })
 }
